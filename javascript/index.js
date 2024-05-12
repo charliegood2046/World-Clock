44bf3318a46ed4dd73af488ba5975f17d95fa050
charliegood2046 <149835864+charliegood2046@users.noreply.github.com>
2024-05-12 00:20:53 -0500

    Javascript for Cardiff & Sendai

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
-//alert("hello!");
+function updateTime() {
+  // Cardiff
+  let cardiffElement = document.querySelector("#cardiff");
+  let cardiffDateElement = cardiffElement.querySelector(".date");
+  let cardiffTimeElement = cardiffElement.querySelector(".time");
+  let cardiffTime = moment().tz("Europe/London");
+
+  cardiffDateElement.innerHTML = cardiffTime.format("MMMM Do, YYYY");
+  cardiffTimeElement.innerHTML = cardiffTime.format("h:mm:ss A");
+
+  // Sendai
+  let sendaiElement = document.querySelector("#sendai");
+  let sendaiDateElement = sendaiElement.querySelector(".date");
+  let sendaiTimeElement = sendaiElement.querySelector(".time");
+  let sendaiTime = moment().tz("Asia/Tokyo");
+
+  sendaiDateElement.innerHTML = sendaiTime.format("MMMM Do, YYYY");
+  sendaiTimeElement.innerHTML = sendaiTime.format("h:mm:ss A");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
